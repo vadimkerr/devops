56692a83db461f9f65e54651e1a86214b54caa03
Vadim Stepanov <vadimkerr@gmail.com>
2021-08-23 13:35:33 +0300

    Initial commit

---
+++ app_python/app.py
@@ -0,0 +1,15 @@
+import pytz as pytz
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def current_time():
+    tz = pytz.timezone("Europe/Moscow")
+    return str(datetime.now().astimezone(tz))
+
+
+if __name__ == "__main__":
+    app.run()
