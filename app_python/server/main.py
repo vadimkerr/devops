de5422249a3bde8cd56ea13b029aed8a4dedfecc
Vadim Stepanov <vadimkerr@gmail.com>
2021-09-27 21:14:55 +0300

    lab 12 configmap part (#9)
    * lab 12 configmap part
    
    * lab 12 visits part

--- app_python/server/main.py
+++ app_python/server/main.py
@@ -1,12 +1,30 @@
 from datetime import datetime
+from pathlib import Path
 
+import aiofiles
 import pytz
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
+file_path = Path("data/last-visited.txt")
 
 
 @app.get("/")
 async def current_time():
     tz = pytz.timezone("Europe/Moscow")
-    return str(datetime.now(tz=tz))
+    now = str(datetime.now(tz=tz))
+
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    async with aiofiles.open(file_path, mode="w") as file:
+        await file.write(now)
+
+    return now
+
+
+@app.get("/visits")
+async def visits():
+    if not file_path.exists():
+        return None
+
+    return FileResponse(file_path)
